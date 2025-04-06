93da0be5364e239370c81e1425d942ff12737a7d
Sumit6307 <sumitkesar6307@gmail.com>
2025-04-06 21:10:51 +0530

    fix: Update API base URL from localhost to production endpoint

--- vite-project/src/utils/api.js
+++ vite-project/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://x-backend-1-zhox.onrender.com/api',
 });
 
 // Request interceptor to add auth token
